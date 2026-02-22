630cc48ddecf24bbe15eb0328a6632e10fd9b9b9
Hariprasad <hariprasadm218@gmail.com>
2026-02-22 15:07:10 +0530

    initial commit

---
+++ TASK-1.py
@@ -0,0 +1,10 @@
+A=int(input('Enter the first number'))
+B=int(input('Enter the second number'))
+#SUM
+print(A+B)
+#DIFFERENCE
+print(A-B)
+#PRODUCT
+print(A*B)
+#DIVISION
+print(A/B)
---
+++ TASK-2.py
@@ -0,0 +1,3 @@
+X=input('Enter the first name: ')
+Y=input('Enter the second name:')
+print(f"Hello Everyone, I am {X} {Y}")
